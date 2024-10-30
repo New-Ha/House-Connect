77ac81163aa8f8540f047488b9424f32bdb849e7
rlwjd31 <rlwjd31@naver.com>
2024-10-31 03:50:53 +0900

    🚀 [Feat]: Render Unknown Error Page
    알 수 없는 오류 페이지는 ErrorFallback component에서 관리.
    
    - 다른 곳에 필요할 시 import

--- src/components/molecules/ErrorFallback.tsx
+++ src/components/molecules/ErrorFallback.tsx
@@ -2,6 +2,7 @@ import { FallbackProps } from 'react-error-boundary';
 
 import Error404 from '@/components/pages/maintenance/Error404';
 import SupabaseCustomError from '@/libs/supabaseCustomError';
+import UnknownError from '@/components/pages/maintenance/UnknownError';
 
 function ErrorFallback({ error }: FallbackProps) {
   const { statusCode } = error as SupabaseCustomError;
@@ -10,8 +11,7 @@ function ErrorFallback({ error }: FallbackProps) {
     return <Error404 />;
   }
 
-  // TODO: unknown error page로 대체
-  return <h1 className="text-8xl text-brown">알 수 없는 오류</h1>;
+  return <UnknownError />;
 }
 
 export default ErrorFallback;
