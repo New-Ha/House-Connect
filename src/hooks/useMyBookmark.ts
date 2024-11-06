0670b3c165de640a5381168f568f7e4209955d0c
rlwjd31 <rlwjd31@naver.com>
2024-11-06 22:34:58 +0900

    🚑️ [Fix]: Resolve house bookmark query
    bookmark된 house를 fetching하는 query가 동작하지 않아 수정 함.
    또한 suspense, error-boundary 사용을 위해 이에 맞게 구현 함.
    
    - storage에 접근하여 image를 가져올 때 user_id가 없어 select('house(*)')로 변경
    - 기존에 user_bookmark table의 id === user.id는 잘못된 query조건으로 수정
    - useMyBookmarkHouseCount는 useMyBookmarkHouseList의 결과 값에 있으므로 불필요해서 제거 함.

--- src/hooks/useMyBookmark.ts
+++ src/hooks/useMyBookmark.ts
@@ -4,6 +4,7 @@ import { UserType } from '@/types/auth.type';
 import { supabase } from '@/libs/supabaseClient';
 import { HouseCardType } from '@/types/house.type';
 import USER_KEYS from '@/constants/queryKeys/user';
+import SupabaseCustomError from '@/libs/supabaseCustomError';
 
 export type HouseBookmarkType = {
   house: HouseCardType;
@@ -16,18 +17,24 @@ export const useMyBookmarkHouseList = (
 ) =>
   queryOptions({
     queryKey: USER_KEYS.USER_BOOKMARK(user?.id, page, filter),
-    queryFn: async () =>
-      supabase
+    queryFn: async () => {
+      const { data, error, status } = await supabase
         .from('user_bookmark')
-        .select(
-          'house(id, representative_img, house_type, rental_type, deposit_price, monthly_price, house_appeal, region, district, term)',
-        )
-        .eq('id', user?.id ?? '')
+        .select('house(*)')
+        .eq('user_id', user?.id ?? '')
         .or(`region.like.%${filter}%,district.like.%${filter}%`, {
           referencedTable: 'house',
         })
-        .range((page - 1) * 9, page * 9),
-    enabled: !!user,
+        .range((page - 1) * 9, page * 9);
+
+      if (error) {
+        throw new SupabaseCustomError(error, status);
+      }
+
+      return data;
+    },
+    // enabled: !!user,
+    throwOnError: true,
   });
 export const useMyBookmarkHouseCount = (
   user: UserType | null,
